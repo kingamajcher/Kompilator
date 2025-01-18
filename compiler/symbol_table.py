20dbda48da889d4ca5e5f49a7975d0b1150e4559
Kinga <272354@student.pwr.edu.pl>
2025-01-18 21:30:11 +0100

    added symbol table and test for it - it works

--- compiler/symbol_table.py
+++ compiler/symbol_table.py
@@ -8,7 +8,7 @@ class Array:
         self.last_index = last_index
 
     def __repr__(self):
-        return f"{self.memory_index}, [{self.first_index}:{self.last_index}]"
+        return f"Array at {self.memory_index}, [{self.first_index}:{self.last_index}]"
     
     def get_at(self, index):
         if index < self.first_index or index > self.last_index:
@@ -22,4 +22,175 @@ class Variable:
         self.initialized = False
 
     def __repr__(self):
-        return f"{self.memory_index}"
+        if self.initialized:
+            return f"Initialized variable at {self.memory_index}"
+        else:
+            return f"Uninitialized variable at {self.memory_index}"
+    
+
+class Iterator:
+    def __init__(self, memory_index, limit_index):
+        self.memory_index = memory_index
+        self.limit_index = limit_index
+
+    def __repr__(self):
+        return f"Iterator at {self.memory_index}"
+    
+
+class Procedure:
+    def __init__(self, name, memory_index, parameters):
+        self.name = name
+        self.memory_index = memory_index
+        self.parameters = parameters
+        self.locals = SymbolTable()
+
+    def __repr__(self):
+        return f"Procedure {self.name} at {self.memory_index} with parameters {self.parameters}"
+    
+
+class SymbolTable(dict):
+    def __init__(self):
+        super().__init__()
+        self.memory_index = 0
+        self.consts = {}
+        self.iterators = {}
+        self.procedures = {}
+
+    # adding variables
+    def add_variable(self, name):
+        if name in self or name in self.iterators:
+            raise ValueError(f"Error: Redeclaration of variable '{name}'.")
+        self[name] = Variable(self.memory_index)
+        self.memory_index += 1
+
+    # adding arrays
+    def add_array(self, name, first_index, last_index):
+        if name in self or name in self.iterators:
+            raise ValueError(f"Error: Redeclaration of array '{name}'.")
+        if first_index > last_index:
+            raise ValueError(f"Invalid range for array '{name}'.")
+        self[name] = Array(name, self.memory_index, first_index, last_index)
+        self.memory_index += (last_index - first_index + 1)
+
+    # adding constants
+    def add_const(self, value):
+        if value not in self.consts:
+            self.consts[value] = self.memory_index
+            self.memory_index += 1
+        return self.consts[value]
+    
+    # adding iterators
+    def add_iterator(self, name):
+        if name in self or name in self.iterators:
+            raise ValueError(f"Error: Redeclaration of iterator '{name}'.")
+        limit_address = self.memory_index + 1
+        self.iterators[name] = Iterator(self.memory_index, limit_address)
+        self.memory_index += 2
+
+    # adding procedure
+    def add_procedure(self, name, start_address, parameters):
+        if name in self.procedures:
+            raise ValueError(f"Error: Redeclaration of procedure '{name}'.")
+        self.procedures[name] = Procedure(name, start_address, parameters)
+
+    # checking if iterator exists
+    def is_iterator(self, name):
+        return name in self.iterators
+    
+    # getting variable
+    def get_variable(self, name):
+        if name in self:
+            return self[name]
+        elif name in self.iterators:
+            return self.iterators[name]
+        else:
+            raise ValueError(f"Error: Undeclared variable '{name}'.")
+        
+    # getting array at value
+    def get_array_at(self, name, index):
+        if name in self:
+            try:
+                return self[name].get_at(index)
+            except AttributeError:
+                raise ValueError(f"Error: Non-array '{name}' used as an array.")
+        else:
+            raise ValueError(f"Error: Undeclared array '{name}'.")
+        
+    # getting address of variable or array
+    def get_address(self, target):
+        if isinstance(target, str):
+            return self.get_variable(target).memory_index
+        elif isinstance(target, tuple):
+            return self.get_array_at(target[0], target[1])
+        
+    # getting procedure
+    def get_procedure(self, name):
+        if name in self.procedures:
+            return self.procedures[name]
+        else:
+            raise ValueError(f"Error: Undeclared procedure'{name}'.")
+        
+def test_symbol_table():
+    # Tworzymy nową tablicę symboli
+    symbol_table = SymbolTable()
+
+    print("=== Dodawanie zmiennych ===")
+    symbol_table.add_variable("x")
+    symbol_table.add_variable("y")
+    print("Tablica symboli po dodaniu zmiennych:")
+    print(symbol_table)
+
+    print("\n=== Dodawanie tablic ===")
+    symbol_table.add_array("arr", -10, 10)
+    symbol_table.add_array("matrix", 0, 4)
+    print("Tablica symboli po dodaniu tablic:")
+    print(symbol_table)
+
+    print("\n=== Pobieranie elementów tablicy ===")
+    print(f"Adres arr[-5]: {symbol_table.get_array_at('arr', -5)}")
+    print(f"Adres matrix[2]: {symbol_table.get_array_at('matrix', 2)}")
+
+    print("\n=== Dodawanie stałych ===")
+    addr1 = symbol_table.add_const(42)
+    addr2 = symbol_table.add_const(100)
+    addr3 = symbol_table.add_const(42)  # Powinna zwrócić ten sam adres co addr1
+    print(f"Adres stałej 42: {addr1}")
+    print(f"Adres stałej 100: {addr2}")
+    print(f"Adres stałej 42 (ponownie): {addr3}")
+
+    print("\n=== Dodawanie iteratorów ===")
+    symbol_table.add_iterator("i")
+    symbol_table.add_iterator("j")
+    print("Tablica symboli po dodaniu iteratorów:")
+    print("Iteratory:", symbol_table.iterators)
+
+    print("\n=== Dodawanie procedury ===")
+    symbol_table.add_procedure(
+        "foo",
+        start_address=100,
+        parameters=[("a", "int"), ("Tarr", "array")]
+    )
+    print(f"Procedura 'foo': {symbol_table.get_procedure('foo')}")
+
+    print("\n=== Pobieranie adresu zmiennej i tablicy ===")
+    print(f"Adres zmiennej 'x': {symbol_table.get_address('x')}")
+    print(f"Adres tablicy 'arr[-5]': {symbol_table.get_address(('arr', -5))}")
+
+    print("\n=== Obsługa błędów ===")
+    try:
+        symbol_table.add_variable("x")  # Próba redeklaracji
+    except Exception as e:
+        print(f"{e}")
+
+    try:
+        symbol_table.get_array_at("matrix", 10)  # Indeks poza zakresem
+    except Exception as e:
+        print(f"{e}")
+
+    try:
+        symbol_table.get_variable("unknown")  # Zmienna niezadeklarowana
+    except Exception as e:
+        print(f"{e}")
+
+if __name__ == "__main__":
+    test_symbol_table()
