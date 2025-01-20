bf2eb3c1c7696f9c6ea3f8fc4f6644998b71ea70
Kinga <272354@student.pwr.edu.pl>
2025-01-20 10:30:03 +0100

    parser that does not return anything but it doesnt retrun error

--- compiler/parser.py
+++ compiler/parser.py
@@ -1,9 +1,10 @@
 from sly import Parser
-
 from lexer import MyLexer
+from symbol_table import Array, Variable, Iterator, SymbolTable
 
 class MyParser(Parser):
     tokens = MyLexer.tokens
+    symbol_table = SymbolTable()
 
     precedence = (
         ('left', 'PLUS', 'MINUS'),
@@ -27,7 +28,7 @@ class MyParser(Parser):
 
     @_('')
     def procedures(self, p):
-        pass
+        return []
 
 
     # main
@@ -53,42 +54,37 @@ class MyParser(Parser):
     # command
     @_('identifier ASSIGN expression SEMICOLON')
     def command(self, p):
-        return "assign", p[0], p[2]
+        temp = "assign", p[0], p[2]
+        return temp
 
     @_('IF condition THEN commands ELSE commands ENDIF')
     def command(self, p):
-        temp = "if_else", p[1], p[3], p[5], self.consts.copy() 
-        self.consts.clear()
+        temp = "if_else", p[1], p[3], p[5]
         return temp
 
     @_('IF condition THEN commands ENDIF')
     def command(self, p):
-        temp = "if", p[1], p[3], self.consts.copy()
-        self.consts.clear()
+        temp = "if", p[1], p[3]
         return temp
 
     @_('WHILE condition DO commands ENDWHILE')
     def command(self, p):
-        temp = "while", p[1], p[3], self.consts.copy()
-        self.consts.clear()
+        temp = "while", p[1], p[3]
         return temp
 
     @_('REPEAT commands UNTIL condition SEMICOLON')
     def command(self, p):
-        temp = "repeat", p[3], p[1], self.consts.copy()
-        self.consts.clear()
+        temp = "repeat", p[3], p[1]
         return temp
 
     @_('FOR PIDENTIFIER FROM value TO value DO commands ENDFOR')
     def command(self, p):
-        temp = "for_to", p[1], p[3], p[5], p[7], self.consts.copy()
-        self.consts.clear()
+        temp = "for_to", p[1], p[3], p[5], p[7]
         return temp
 
     @_('FOR PIDENTIFIER FROM value DOWNTO value DO commands ENDFOR')
     def command(self, p):
-        temp = "for_downto", p[1], p[3], p[5], p[7], self.consts.copy()
-        self.consts.clear()
+        temp = "for_downto", p[1], p[3], p[5], p[7]
         return temp
 
     @_('proc_call SEMICOLON')
@@ -97,11 +93,12 @@ class MyParser(Parser):
 
     @_('READ identifier SEMICOLON')
     def command(self, p):
-        return "read", p[1]
+        temp = "read", p[1]
+        return temp
 
     @_('WRITE value SEMICOLON')
     def command(self, p):
-        pass
+        temp = "write", p[1]
 
 
     # proc_head
@@ -221,21 +218,33 @@ class MyParser(Parser):
 
     @_('identifier')
     def value(self, p):
-        return "id", p[0]
+        return "load", p[0]
 
 
     # identifier
     @_('PIDENTIFIER')
     def identifier(self, p):
-        return "pid", p[0]
+        if p[0] in self.symbol_table or p[0] in self.symbol_table.iterators:
+            return p[0]
+        else:
+            return "undeclared", p[0]
 
     @_('PIDENTIFIER LBRACKET PIDENTIFIER RBRACKET')
     def identifier(self, p):
-        pass
+        if p[0] in self.symbol_table and type(self.symbol_table[p[0]]) == Array:
+            if p[2] in self.symbol_table and type(self.symbol_table[p[2]]) == Variable:
+                return "array", p[0], ("load", p[2])
+            else:
+                return "array", p[0], ("load", ("undeclared", p[2]))
+        else:
+            raise Exception(f"Unknown array {p[0]} at {p.lineno}")
 
     @_('PIDENTIFIER LBRACKET NUM RBRACKET')
     def identifier(self, p):
-        pass
+        if p[0] in self.symbol_table and type(self.symbol_table[p[0]]) == Array:
+            return "array", p[0], p[2]
+        else:
+            raise Exception(f"Unknown array {p[0]} at {p.lineno}")
 
 
     # error
@@ -244,3 +253,27 @@ class MyParser(Parser):
             print(f'Syntax error: {p.type}')
         else:
             print("Syntax error at EOF")
+
+program = '''PROGRAM IS
+    x, y, f[0:1]
+BEGIN
+    x := 10;
+    y := x + 2;
+    IF x < y THEN
+        WRITE x;
+    ELSE
+        WRITE y;
+    ENDIF
+END'''
+
+if __name__ == "__main__":
+    lexer = MyLexer()
+    parser = MyParser()
+
+    try:
+        tokens = lexer.tokenize(program)
+        result = parser.parse(tokens)
+        print("AST:")
+        print(result)
+    except Exception as e:
+        print(f"Error: {e}")
